5e12d0704932d88e3282ccf77cb56e39bdbf55c1
Lonnie Gainey <autoptic2012@gmail.com>
2021-04-23 20:49:37 -0400

    second push to add substrings

---
+++ substrings.rb
@@ -0,0 +1,22 @@
+def substrings (word, dictionary)
+    match_list = []
+    match_score = {}
+    
+    dictionary.each do |string|
+        match_list += word.downcase.scan(string)
+    end
+
+    match_list.each do |i|
+        if (match_score.key?(i))
+            match_score[i] += 1
+        else
+            match_score.store(i, 1)
+        end
+    end
+    return match_score
+end
+
+#Neither was an interface specified nor any exception catching expected, so the below was just for demonstration.
+string = "Howdy partner, sit down! How's it going?"
+array = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+pp substrings(string, array)
